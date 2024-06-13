5aecdf131dc7c03b42dda7706b4a4e30d755907e
chen <kagaminele@qq.com>
2024-06-13 10:53:45 +0800

    change dir

---
+++ home/snake/precache-manifest.7642177cba112983bcc54f5149878db0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a96d1d4d2fd67e283ba477c86abc6d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5141353e89743086e4dc",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c331765cda579436fbf5",
+    "url": "/static/js/2.af4fb9b9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.af4fb9b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5141353e89743086e4dc",
+    "url": "/static/js/main.f9e5c065.chunk.js"
+  },
+  {
+    "revision": "08cdd3b62cb2c7c134b2",
+    "url": "/static/js/runtime-main.0ac8b321.js"
+  }
+]);
